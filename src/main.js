7f8b3c6964215644f5c793b50ba9ea825000eec7
Kaspars Dambis <hi@kaspars.net>
2019-03-03 19:00:45 +0200

    Two spaces please

--- src/main.js
+++ src/main.js
@@ -6,21 +6,21 @@
  * @return {string}
  */
 function format( content ) {
-	const replaceRules = {
-		'^#\\s(.+)$': '=== $1 ===',
-		'^##\\s(.+)$': '== $1 ==',
-		'^#{3,}\\s(.+)$': '= $1 =',
-	};
+  const replaceRules = {
+    '^#\\s(.+)$': '=== $1 ===',
+    '^##\\s(.+)$': '== $1 ==',
+    '^#{3,}\\s(.+)$': '= $1 =',
+  };
 
-	// Replace Markdown headings with WP.org style headings.
-	Object.keys( replaceRules ).forEach( pattern => {
-		content = content.replace(
-			new RegExp( pattern, 'gm' ),
-			replaceRules[ pattern ]
-		);
-	} );
+  // Replace Markdown headings with WP.org style headings.
+  Object.keys( replaceRules ).forEach( pattern => {
+    content = content.replace(
+      new RegExp( pattern, 'gm' ),
+      replaceRules[ pattern ]
+    );
+  } );
 
-	return content;
+  return content;
 }
 
 export default format
